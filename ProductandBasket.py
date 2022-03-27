408c8b9b859499cfe98c9dd059b47cdf75e04c39
sirius <sofja.iakovleva@gmail.com>
2022-03-27 16:48:56 +0300

    tests

--- ProductandBasket.py
+++ ProductandBasket.py
@@ -7,13 +7,17 @@ class Product():
         self.price = price
 
 class Basket:
-    basket: List[Product] = []
+    def __init__(self):
+        self.basket: List[Product] = []
     def add(self, product: Product):
         self.basket.append(product)
+        return 'add'
     def remove(self, product: Product):
         self.basket.remove(product)
+        return 'remove'
     def count_sum(self):
         summ = 0
         for i in range(0, len(self.basket)):
             summ += self.basket[i].price
         return summ
+
--- test_add.py
+++ test_add.py
@@ -1,16 +1,8 @@
-from ProductandBasket import Basket, Product
+from ProductandBasket import *
 import pytest
 
-# tests = [('Мармелад', 'Мармелад'), ('Картофель', 'Картофель'), ('Морковь', 'Морковь')]
-# tests = [('Мармелад, Картофель', 'Мармелад, Картофель')]
-# tests = [('Мармелад', 'Мармелад')]
-# @pytest.mark.parametrize('product,expected', tests)
-# def test_add(product, expected):
-#     assert Basket.add(product) == expected
 
-
-
-test1 = [([Product('Хлеб', 50.0), Product('Морковь', 20.0), Product('Липтон', 100.0)], 'add')]
+test1 = [([Product('bread', 50.0), Product('carrot', 20.0), Product('tea', 100.0)], 'add')]
 @pytest.mark.parametrize('product, s', test1)
 def test_add(product: Product, s: str):
     basket1 = Basket()
@@ -18,39 +10,39 @@ def test_add(product: Product, s: str):
         basket1.add(p)
         assert s == basket1.add(p)
 
-test2 = [([Product('Хлеб', 50.0), Product('Морковь', 20.0), Product('Липтон', 100.0)], 'remove')]
+test2 = [([Product('bread', 50.0), Product('carrot', 20.0), Product('tea', 100.0)], 'remove')]
 @pytest.mark.parametrize('product, s', test2)
 def test_remove(product: Product, s: str):
     basket2 = Basket()
     for p in product:
-        basket2.remove(p)
-        assert s == basket2.remove(p)
+        basket2.add(p)
+    assert s == basket2.remove(p)
 
-test3 = [([Product('Хлеб', 50.0), Product('Морковь', 20.0), Product('Липтон', 100.0)], '170.0')]
+test3 = [([Product('bread', 50.0), Product('carrot', 20.0), Product('tea', 100.0)], 170.0)]
 @pytest.mark.parametrize('product, s', test3)
-def test_summ(product: Product, s: str):
+def test_summ(product: Product, s: float):
     basket3 = Basket()
     for p in product:
         basket3.add(p)
-        assert s == basket3.count_sum(p)
+    assert s == basket3.count_sum()
 
-test4 = [([Product('Хлеб', 50.0), Product('Морковь', 20.0), Product('Липтон', 100.0)])]
-
+test4 = [('bread', 50.0), ('carrot', 20.0)]
 def test_field():
-    bread = Product('Хлеб', 50.0)
-    assert bread.name == 'Хлеб'
+    bread = Product('bread', 50.0)
+    assert bread.name == 'bread'
 
 
 @pytest.mark.parametrize('name, price', test4)
 def test_name(name: str, price: float):
     field_name = Product(name, price)
-    assert field_name == name
+    assert field_name.name == name
 
 
 @pytest.mark.parametrize('name, price', test4)
 def test_price(name: str, price: float):
     field_price = Product(name, price)
-    assert field_price == price
+    assert field_price.price == price
+
 
 
 
